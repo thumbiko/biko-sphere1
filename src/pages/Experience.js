9ca6d830983c45bc2c2c3f5cb986eda886c59f9a
thumbiko nyasulu <thumbikonyasulu9@gmail.com>
2025-12-12 09:46:32 +0000

    Testing hosting

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,19 +4,9 @@ import "../styles/Experience.css";
 function Experience() {
   const cvPath = process.env.PUBLIC_URL + "/cv.pdf";
 
-  const openCv = () => {
-    window.open(cvPath, "_blank");
-  };
-
   return (
     <div className="experience-page">
       <h1>My Experience</h1>
-
-      <div style={{ margin: "20px 0" }}>
-        <button onClick={openCv} style={styles.button}>
-          Open My CV in a new tab
-        </button>
-      </div>
 
       <div className="cv-display">
         <embed
@@ -29,18 +19,5 @@ function Experience() {
     </div>
   );
 }
-
-const styles = {
-  button: {
-    backgroundColor: "#4e169e",
-    color: "white",
-    border: "none",
-    padding: "10px 20px",
-    fontSize: "16px",
-    cursor: "pointer",
-    borderRadius: "5px",
-    transition: "background-color 0.3s",
-  },
-};
 
 export default Experience;
