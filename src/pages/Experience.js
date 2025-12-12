b00788569d5bc20170bd05792204d28f0611114e
thumbiko nyasulu <thumbikonyasulu9@gmail.com>
2025-12-12 10:19:24 +0000

    testing

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "../styles/Experience.css";
+import cvFile from "../assets/cv.pdf"; // <-- IMPORT THE PDF
 
 function Experience() {
-  const cvPath = process.env.PUBLIC_URL + "/cv.pdf";
-
   return (
     <div className="experience-page">
       <h1>My Experience</h1>
 
       <div className="cv-display">
         <embed
-          src={cvPath}
+          src={cvFile}
           width="100%"
           height="600px"
           type="application/pdf"
